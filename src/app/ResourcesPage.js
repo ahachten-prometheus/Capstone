259133530c7ad781380c4ef7e2d85581bb80df3a
embryobirthday <alexinverda@gmail.com>
2025-05-28 14:02:17 -0400

    Made resources page and planned out components

---
+++ src/app/ResourcesPage.js
@@ -0,0 +1,15 @@
+export default function Resources() {
+    return (
+        <div>
+            <h2> Resources </h2>
+            {/* resource page recommendation block component */}
+            {/* Picture-slideshow component */}
+
+            <h2>All Resources</h2> {/* Changes if there are filters present. Otherwise, shows "all filters" */}
+            {/* Search bar component */}
+            {/* filter drop downs */}
+            {/* Resource grid */}
+            {/* Load more button */}
+        </div>
+    )
+}
