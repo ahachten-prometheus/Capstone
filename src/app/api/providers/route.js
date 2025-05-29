405e0719a10c630ea9f1fe900eed477e08c9fa87
ChrisAlJim <jimenezaot@gmail.com>
2025-05-29 15:55:37 -0400

    Added comment to providers route

--- src/app/api/providers/route.js
+++ src/app/api/providers/route.js
@@ -1,13 +1,13 @@
 import { getRecords } from "@/app/services/airtable";
 
-// An example API endpoint, showing how to call getRecords
+// gets all active providers
 export async function GET(request) {
   try {
     const data = await getRecords({
       tableName: "Providers",
       filters: "FIND('Active', {Status})"
     });
-    console.log(data)
+
     return Response.json(data);
   } catch (error) {
     console.error(error);
