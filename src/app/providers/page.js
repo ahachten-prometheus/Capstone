0986522300f3030a37ade70b9085bff1b499c4a6
Kelly Xiong Chen <kellyxiongc@gmail.com>
2025-05-29 15:57:35 -0400

    Building/Testing Drop-down Selections

--- src/app/providers/page.js
+++ src/app/providers/page.js
@@ -1,9 +1,33 @@
+"use client";
+import { useState } from "react";
+
 export default function Providers() {
+  const [selectedState, setState] = useState("");
+  const [selectedMode, setMode] = useState("");
+
+  const usStates = [
+    "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
+    "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+    "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+    "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+    "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"
+  ];
+
+  const mockData = [
+    { id: 1, name: "name1", practice: "something1", state: "", isVirtual: "" },
+    { id: 2, name: "name2", practice: "something2", state: "NY", isVirtual: "Yes" },
+    { id: 3, name: "name3", practice: "something3", state: "NY", isVirtual: "No" },
+    { id: 4, name: "name4", practice: "something4", state: "HI", isVirtual: "Yes" }
+  ];
 
   // have what the user selects here, filter what provider tiles are shown 
-  // const providerState = (event) => {
+  const handleStateChange = (event) => {
+    setState(event.target.value);
+  }
 
-  // }
+  const handleModeChange = (event) => {
+    setMode(event.target.value);
+  }
 
   return <>
     {/* hero header */}
@@ -13,29 +37,39 @@ export default function Providers() {
       {/* above the tiles section */}
       <h3>Find a Provider</h3>
       <section id="providers-filters">
-        {/* <select id="provider-state" onChange={providerState()} aria-labelledby="filtering-state"> */}
-        <select id="provider-state" aria-labelledby="filtering-state">
-          {/* use .map() on these?  */}
-          <option value="State">State?</option>
-          <option value="State1">State1</option>
-          <option value="State2">State2</option>
-          <option value="State3">State3</option>
+        <p>State:</p>
+        <select id="provider-state" onChange={handleStateChange} aria-labelledby="filtering-state">
+          <option value="--Select a State--" selected>--Select a State--</option>
+          { usStates.map((state) => <option value={state}>{state}</option>) }
         </select>
+        <p id="test">You selected this state: {selectedState}</p>
         
-        {/* <select id="provider-mode" onChange={providerMode()} aria-labelledby="filtering-mode"> */}
-        <select id="provider-mode" aria-labelledby="filtering-mode">
-          <option value="Virtual Only" selected>Virtual Only?</option>
+        <select id="provider-mode" onChange={handleModeChange} aria-labelledby="filtering-mode">
+          <p>Virtual Only?:</p>
+          <option value="--Select a Mode--" selected>--Select a Mode--</option>
           <option value="Yes">Yes</option>
           <option value="No">No</option>
         </select>
+        <p id="test">You selected this mode: {selectedMode}</p>
       </section>
       
       {/* tiles section */}
+      {/* next: filtering this */}
       <section id="providers-display">
-
+        { mockData.map((data) => {
+          return (
+            <ul>
+              <li>{data.name}</li>
+              <li>{data.practice}</li>
+              <li>{data.state}</li>
+              <li>{data.isVirtual}</li>
+            </ul>
+          )
+        }) }
       </section>
 
-      <button>Load More</button>
+      {/* next: load more providers using this button */}
+      <button id="more-providers">Load More</button>
     </main>
 
   </>;
