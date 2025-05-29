3df93402c0178c194e001eaa09935d085d57cb62
embryobirthday <alexinverda@gmail.com>
2025-05-29 12:31:57 -0400

    created a resource file in src/app, added page.js with basic component planning and an empty layout.js file.

---
+++ src/app/resources/layout.js
@@ -0,0 +1,8 @@
+// export default function RoleBasedLayout({ children, params }) {
+//     return (
+//         <div className={`layout-${params.role}`}>
+//             <header>{params.role} Header</header>
+//             <main>{children}</main>
+//         </div>
+//     );
+// }
--- src/app/resources/page.js
+++ src/app/resources/page.js
@@ -1,15 +1,21 @@
-export default function Resources() {
+export default function resources() {
     return (
         <div>
             <h2> Resources </h2>
-            {/* resource page recommendation block component */}
-            {/* Picture-slideshow component */}
+            <div className="resource-suggestion-flag">
+                {/* resource page recommendation block component */}
+            </div>
+
+            <div className="resource-image">
+                {/* Picture-slideshow component */}
+            </div>
 
             <h2>All Resources</h2> {/* Changes if there are filters present. Otherwise, shows "all filters" */}
             {/* Search bar component */}
             {/* filter drop downs */}
             {/* Resource grid */}
             {/* Load more button */}
+
         </div>
     )
 }
