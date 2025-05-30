ce07be8697c486b38cb90223f4e8146383d69957
William Zeng <itswilliamzeng@gmail.com>
2025-05-30 11:29:34 -0400

    optimized-events-fetch-handler

--- src/app/api/events/route.js
+++ src/app/api/events/route.js
@@ -2,33 +2,16 @@ import { getRecords } from '../../services/airtable';
 
 export async function GET() {
 	try {
-		//fetch records --> airtable calls their entries 'records' so for clarity we too will use {records}. Here, records = events
+		//fetch records --> airtable calls their entries 'records'.
 		const { records: events } = await getRecords({
 			tableName: 'Events',
 			filters: `{Status} = "Approved"`,
 		});
-		//if successful return in response
-		return new Response(JSON.stringify(events), {
-			status: 200,
-			headers: { 'Content-Type': 'application/json' },
-		});
-		//if issue then ---> console.log the error with records(events)for debugging
+		return Response.json(events, { status: 200 });
 	} catch (error) {
 		console.error(
 			`Encountered ${error} when trying to fetch records from Airtable`
 		);
-		//generic failure message to the client/frontend.
-		return new Response(
-			JSON.stringify({
-				error: 'Failed to fetch events',
-			}),
-			{ status: 500 }
-		);
+		return Response.json({ error: 'Failed to fetch events' }), { status: 500 };
 	}
 }
-
-/*
-Cleaner but no error handling
-	const { records } = await getRecords({ tableName: 'Events' });
-	return Response.json(records);
-*/
