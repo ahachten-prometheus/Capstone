d7b08d586fde46cb3c65b72d6a365a74e71c40d0
Kelly Xiong Chen <kellyxiongc@gmail.com>
2025-05-30 13:11:55 -0400

    Before Implementing Tailwind CSS

--- src/app/providers/page.js
+++ src/app/providers/page.js
@@ -13,14 +13,7 @@ export default function Providers() {
     "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"
   ];
 
-  const mockData = [
-    { id: 1, name: "name1", practice: "something1", state: "", isVirtual: "" },
-    { id: 2, name: "name2", practice: "something2", state: "NY", isVirtual: "Yes" },
-    { id: 3, name: "name3", practice: "something3", state: "NY", isVirtual: "No" },
-    { id: 4, name: "name4", practice: "something4", state: "HI", isVirtual: "Yes" }
-  ];
-
-  // have what the user selects here, filter what provider tiles are shown 
+  // for testing - making sure we can grab the selection value(s) from user 
   const handleStateChange = (event) => {
     setState(event.target.value);
   }
@@ -31,42 +24,32 @@ export default function Providers() {
 
   return <>
     {/* hero header */}
-    <h1>Providers</h1> 
+    <h1 id="providers-hero-header">Providers</h1> 
 
     <main>
       {/* above the tiles section */}
-      <h3>Find a Provider</h3>
+      {/* next: clear selections option */}
+      <h3 id="providers-tiles-header">Find a Provider</h3>
       <section id="providers-filters">
-        <p>State:</p>
-        <select id="provider-state" onChange={handleStateChange} aria-labelledby="filtering-state">
-          <option value="--Select a State--" selected>--Select a State--</option>
-          { usStates.map((state) => <option value={state}>{state}</option>) }
+        {/* <p>State:</p> */}
+        <select id="provider-state" defaultValue="blank-state-opt"onChange={handleStateChange} aria-labelledby="filtering-state">
+          <option key="blank-state-opt" value="blank-state-opt" disabled>State?</option>
+          { usStates.map((state) => <option key={`${state}-option`} value={state}>{state}</option>) }
         </select>
-        <p id="test">You selected this state: {selectedState}</p>
+        {/* <p id="test">You selected this state: {selectedState}</p> */}
         
-        <select id="provider-mode" onChange={handleModeChange} aria-labelledby="filtering-mode">
-          <p>Virtual Only?:</p>
-          <option value="--Select a Mode--" selected>--Select a Mode--</option>
-          <option value="Yes">Yes</option>
-          <option value="No">No</option>
+        {/* <p>Virtual Only?:</p> */}
+        <select id="provider-mode" defaultValue="blank-mode-opt" onChange={handleModeChange} aria-labelledby="filtering-mode">
+          <option key="blank-mode-opt" value="blank-mode-opt" disabled>Virtual Only?</option>
+          <option key="yes-opt" value="Yes">Yes</option>
+          <option key="no-opt" value="No">No</option>
         </select>
-        <p id="test">You selected this mode: {selectedMode}</p>
+        {/* <p id="test">You selected this mode: {selectedMode}</p> */}
       </section>
       
       {/* tiles section */}
       {/* next: filtering this */}
-      <section id="providers-display">
-        { mockData.map((data) => {
-          return (
-            <ul>
-              <li>{data.name}</li>
-              <li>{data.practice}</li>
-              <li>{data.state}</li>
-              <li>{data.isVirtual}</li>
-            </ul>
-          )
-        }) }
-      </section>
+      <section id="providers-display"></section>
 
       {/* next: load more providers using this button */}
       <button id="more-providers">Load More</button>
