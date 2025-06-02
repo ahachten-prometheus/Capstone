8a548aaeabbf631cfae26e8b696d308510c5d33e
Kevin <mrkev63@gmail.com>
2025-06-02 19:33:33 -0400

    Landing page is done, edited the navbar and footer to match figma

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,19 +1,23 @@
 // Super barebones Footer. Will be at the bottom of every page
+import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="bg-white text-black border-t">
-      <div className="max-w-2xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
-        <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 text-center sm:text-left">
+    <footer className="bg-[#FFF5EA] text-black border-t">
+      <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-20 text-center sm:text-left">
           {/* Left Column */}
-          <div>
-            <p className="font-semibold">Black Girls Smile</p>
+          <div className="flex flex-col justify-center align-center gap-5">
+            <div className="flex align-center justify-center">
+              <Image src="/BGSLogo.png" alt="Black-Girls-Smile-Logo" width={150} height={100} />
+            </div>
+            <button type="button" className="bg-[#DCAD27] text-white rounded-full py-1 px-3 cursor-pointer text-[14px]">Contact Us</button>
           </div>
 
           {/* Middle Column */}
-          <div>
+          <div className="flex flex-col align-center gap-2">
             <p className="font-semibold mb-2">Quick Links</p>
-            <ul className="space-y-1">
+            <ul className="flex flex-col gap-2 space-y-1">
               <li>
                 <a href="/membership" className="hover:underline">
                   Membership
@@ -34,8 +38,8 @@ export default function Footer() {
 
           {/* Right Column */}
           <div>
-            <p className="font-semibold mb-2">Learn More</p>
-            <ul className="space-y-1">
+            <p className="flex flex-col align-center gap-2 font-semibold mb-2">Learn More</p>
+            <ul className="flex flex-col gap-2 space-y-1">
               <li>
                 <a href="/join" className="hover:underline">
                   Join Our Team
