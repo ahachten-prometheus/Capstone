e16e730e60b37c920833ef8fcaa45d16e4683afb
Taifinkaba <taifinkabad@gmail.com>
2025-06-03 14:58:01 -0400

    providers display card is in providers page  and uses the grid formation, next step is to connect the card to the api route

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,6 @@
-import ProvidersDisplayCard from "@/components/ProvidersDisplayCard";
 
 export default function Home() {
   return <div>
     <h1>Hello</h1>
-    <ProvidersDisplayCard />
   </div>;
 }
