ca6f5251e73b05f4a18545bffa7186f70110f0fc
madisonnnn <madisontolentino@gmail.com>
2025-06-04 14:41:41 -0400

    api integration-madison

---
+++ src/app/api/resources/highlighted/route.js
@@ -0,0 +1,22 @@
+import { getRecords } from "@/services/airtable";
+//api/resources/highlighted
+export async function GET(req) {
+  const params = req.nextUrl.searchParams;
+
+  const pageSize = params.get("pageSize");
+  const offset = params.get("offset");
+
+  try {
+    const data = await getRecords({
+      tableName: "Resources",
+      filters: `{Resource Highlight} = TRUE()`,
+      pageSize: pageSize ?? 8,
+      offset: offset,
+    });
+
+    return Response.json(data);
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Error!" });
+  }
+}
