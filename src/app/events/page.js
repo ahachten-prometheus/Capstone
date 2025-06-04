a1e9e8c0492d4c263645bb68279bf133e802f411
Sleepy-kookoo <americaxalvarez@gmail.com>
2025-06-04 08:52:08 -0400

    Added Header with styling

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -3,6 +3,12 @@
 export default function Events() {
 
   return <>
+    {/* Header */}
+    <section 
+      className="w-screen, w-[1440px], h-[407px] flex items-center justify-center"
+      style={{ backgroundColor: '#C96C86B0' }}>
+      <h1 id="events-hero-header" className="text-[2.5rem] font-bold text-white"> Upcoming Events</h1> 
+    </section>
 
   </>;
 }
