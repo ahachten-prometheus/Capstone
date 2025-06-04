aa66da26ed1c5c821c9d19edcac99f79e5bf5185
Sleepy-kookoo <americaxalvarez@gmail.com>
2025-06-04 08:56:14 -0400

    Added main body and tiles header with styling

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -10,5 +10,18 @@ export default function Events() {
       <h1 id="events-hero-header" className="text-[2.5rem] font-bold text-white"> Upcoming Events</h1> 
     </section>
 
+    {/* Main Body */}
+    <main 
+      className=" text-black, flex flex-col items-center justify-center" 
+      style={{ backgroundColor: '#FFF5EA' }}>
+
+      {/* Tiles Header */}
+      <h3 
+      id="events-tiles-header" 
+      className=" w-4/5 mx-auto, border-b-1 border-black, text-2xl font-bold text-black , flex justify-center pt-8">
+      Upcoming Events & Webinar </h3>
+      
+      </main>
+
   </>;
 }
