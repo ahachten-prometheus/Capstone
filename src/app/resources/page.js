ad1f4efaa1fa98090fbe396c1e3f9ff7c29d463d
embryobirthday <alexinverda@gmail.com>
2025-06-04 14:38:01 -0400

    Resources grid loaded onto resources page.js

--- src/app/resources/page.js
+++ src/app/resources/page.js
@@ -1,10 +1,37 @@
+"use client";
 import ResourceTileGrid from "@/components/ResourceTileGrid"
+import { useState, useEffect } from "react"
+import React from "react";
 
-export default function resources() {
+export default function Resources() {
+    const [resources, setResources] = useState([])
+    const [offset, setOffset] = useState(null)
+    const [highlightedResources, setHighlighted] = useState([])
+    const [highlightedOffset, setHighOffset] = useState(null)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const [data, error] = await fetchResources()
+
+                if (data) {
+                    setResources(data.records);
+                    setOffset(data.offset)
+                }
+
+            } catch (err) {
+                setError(err.message)
+            }
+        }
+
+        fetchData()
+    }, [])
+
     return (
         <div>
             <h2> Resources </h2>
-            <div className="resource-suggestion-flag">
+            <div className="highlighted-resource-block">
                 {/* resource page recommendation block component */}
             </div>
 
@@ -12,12 +39,54 @@ export default function resources() {
                 {/* Picture-slideshow component */}
             </div>
 
-            <h2>All Resources</h2> {/* Changes if there are filters present. Otherwise, shows "all filters" */}
-            {/* Search bar component */}
-            {/* filter drop downs */}
-            <ResourceTileGrid />
-            {/* Load more button */}
+            <div className="all-resources">
+                <h2>All Resources</h2> {/* Changes if there are filters present. Otherwise, shows "all filters" */}
+                {/* Search bar component */}
+                {/* filter drop downs */}
+                <ResourceTileGrid resources={resources} />
+                {/* Load more button */}
+
+            </div>
 
         </div>
     )
 }
+
+async function fetchResources({ pageSize = 8, offset } = { pageSize: 8 }) {
+
+    try {
+        const params = new URLSearchParams()
+        params.append('pageSize', pageSize.toString())
+        if (offset) params.append('offset', offset.toString())
+
+        const response = await fetch(`/api/resources?${params.toString()}`);
+        const data = await response.json();
+
+        if (!response.ok) return data.err
+        return [data, null]
+
+    } catch (err) {
+        console.error("unable to fetch resources", err)
+        return [null, err]
+    }
+}
+
+async function fetchHighlightedResources({ pageSize = 1, offset } = { pageSize: 1 }) {
+
+    try {
+        const params = new URLSearchParams()
+        params.append('pageSize', pageSize.toString())
+        if (offset) params.append('offset', offset.toString())
+
+        const response = await fetch(`/api/resources/highlighted?${params.toString()}`);
+        const data = await response.json();
+
+        if (!response.ok) return data.err
+        return [data, null]
+
+    } catch (err) {
+        console.error("unable to fetch highlighted resources", err)
+        return [null, err]
+    }
+}
+
