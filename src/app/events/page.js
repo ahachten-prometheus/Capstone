77037f7582ea430e1c1e47e038b6998b8d06a0ee
Sleepy-kookoo <americaxalvarez@gmail.com>
2025-06-05 09:38:59 -0400

    Taken in changes and made all changes

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -4,14 +4,14 @@ import { useState, useEffect } from "react";
 export default function Events() {
   const [events, setEvents] = useState([]);
 
+  async function fetchEvents() {
+    const res = await fetch('/api/events');
+    const data = await res.json();
+    console.log(data);
+    setEvents(data);
+  }
+
   useEffect(() => {
-    async function fetchEvents() {
-      const res = await fetch('/api/events');
-      const data = await res.json();
-      console.log(data);
-      setEvents(data)
-    }
-  
     fetchEvents();
   }, []);
 
@@ -22,26 +22,26 @@ export default function Events() {
   return <>
     {/* Header */}
     <section 
-      className="w-screen, w-[1440px], h-[407px] flex items-center justify-center"
+      className="w-screen w-[1440px] h-[407px] flex items-center justify-center"
       style={{ backgroundColor: '#C96C86B0' }}>
       <h1 id="events-hero-header" className="text-[2.5rem] font-bold text-white"> Upcoming Events</h1> 
     </section>
 
     {/* Main Body */}
     <main 
-      className=" text-black, flex flex-col items-center justify-center" 
+      className=" text-black flex flex-col items-center justify-center" 
       style={{ backgroundColor: '#FFF5EA' }}>
 
       {/* Tiles Header */}
       <h3 
       id="events-tiles-header" 
-      className=" w-4/5 mx-auto, border-b-1 border-black, text-2xl font-bold text-black , flex justify-center pt-8">
+      className=" w-4/5 mx-auto border-b-1 border-black text-2xl font-bold text-black flex justify-center pt-8">
       Upcoming Events & Webinar </h3>
 
       {/* Tiles*/}
       <section 
       id="events-display" 
-      className="pt-10 pb-10 pl-130 pr-130">
+      className="py-10 px-[130px]">
         <p>--Tiles here--</p>
 
       {/* Testing API*/}
@@ -55,7 +55,7 @@ export default function Events() {
       {/* Load More Button*/}
       <button 
       id="more-events"
-      className=" bg-[#B36078] hover:bg-[#C96C86B0], text-white font-bold, py-2 px-4 rounded-full, m-6"
+      className=" bg-[#B36078] hover:bg-[#C96C86B0] text-white font-bold py-2 px-4 rounded-full m-6"
       onClick={handleLoadMore}>
         Load More
       </button>
