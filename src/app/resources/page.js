f1be0c32bec7ec7a18adcc3ab8ec47c0bc2ec8e4
embryobirthday <alexinverda@gmail.com>
2025-06-05 18:11:26 -0400

    Added pagination functions (load more button)

--- src/app/resources/page.js
+++ src/app/resources/page.js
@@ -18,6 +18,7 @@ export default function Resources() {
                 if (data) {
                     setResources(data.records);
                     setOffset(data.offset)
+                    console.log(offset)
                 }
 
             } catch (err) {
@@ -28,8 +29,25 @@ export default function Resources() {
         fetchData()
     }, [])
 
+    //button function to render more resourceess
+    const handleLoadMoreClick = async (event) => {
+        // event.preventDefault() //don't refresh the page pls 
+        try {
+            const [data, error] = await fetchResources({ offset })
+            if (data) {
+                setResources([...resources, ...data.records]);
+                setOffset(data.offset)
+                console.log(offset)
+            }
+        } catch (err) {
+            setError(err.message)
+        }
+
+    }
+
     return (
         <div>
+
             <h2> Resources </h2>
             <div className="highlighted-resource-block">
                 {/* resource page recommendation block component */}
@@ -45,6 +63,7 @@ export default function Resources() {
                 {/* filter drop downs */}
                 <ResourceTileGrid resources={resources} />
                 {/* Load more button */}
+                <button onClick={() => handleLoadMoreClick(offset)} className="bg-[#C96C86] hover:bg-[#8F5E72] cursor-pointer px-4 py-2 ">load more</button>
 
             </div>
 
@@ -53,6 +72,8 @@ export default function Resources() {
 }
 
 async function fetchResources({ pageSize = 8, offset } = { pageSize: 8 }) {
+
+    console.log("fetching resources...")
 
     try {
         const params = new URLSearchParams()
