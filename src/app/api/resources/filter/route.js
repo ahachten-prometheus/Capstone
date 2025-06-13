9e708e23533d43206df5f28df7d3b69cdb7febed
Mekhi-M <mekhim0731@gmail.com>
2025-06-13 14:16:57 -0400

    API Route:  Filter capabilty improvements
    Implemented RegEx_Match formulas to improve filtering capabilities

--- src/app/api/resources/filter/route.js
+++ src/app/api/resources/filter/route.js
@@ -3,20 +3,28 @@ import { getRecords } from "@/services/airtable";
 export async function GET(req) {
   const params = req.nextUrl.searchParams;
 
+  // params.append("pageSize", 100);
+  // params.append("filters", ["Subject", "Anxiety & Depression"]);
+  // params.append("filters", ["Subject", "Bipolar Disorder"]);
+  // params.append("filters", ["Subject", "Eating Disorder"]);
+
   const pageSize = params.get("pageSize");
   const offset = params.get("offset");
   const filters = params.getAll("filters");
 
   const filterFormulas = filters.map(filter => {
-    const idxToSplit = filter.findIndex(",");
+    const idxToSplit = filter.indexOf(",");
 
     const field = filter.slice(0, idxToSplit);
     const value = filter.slice(idxToSplit + 1);
 
-    if (field === "Name" || field === "Subject") {
+    if (field === "Subject") {
       // TODO
-      // RegEx matching
-      return `REGEX_MATCH({${field}}, '(${value})')`;
+      // Test RegEx matching
+      const regex = value.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+      // console.log(regex);
+
+      return `REGEX_MATCH(LOWER({${field}}),LOWER('(${regex})'))`;
     } else {
       return `{${field}} = '${value}'`;
     }
@@ -25,8 +33,9 @@ export async function GET(req) {
   try {
     const [data, error] = await getRecords({
       tableName: "Resources",
-      // filters: `AND({Status} = 'Active', ${filterFormulas.join(', ')})`,
-      filters: `AND({Status} = 'Active', {Subject} = "Anxiety & Depression")`,
+      filters: `AND({Status}='Active',${
+        filterFormulas.length > 0 ? filterFormulas.toString() : "TRUE()"
+      })`,
       pageSize: pageSize ?? 8,
       offset: offset,
     });
