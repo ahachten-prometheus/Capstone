189bb7e5dec869ca3e2d04dac5ec5d50756686ae
Mekhi-M <mekhim0731@gmail.com>
2025-06-14 13:33:15 -0400

    Refactoring Resource Tiles

--- src/components/ResourceTiles.js
+++ src/components/ResourceTiles.js
@@ -1,16 +1,32 @@
 export default function ResourceTiles({ resource }) {
-  const { Name, ["Resources Type"]: Resources_Type, Description, URL, imgURL } = resource
+  const {
+    Name,
+    Status,
+    ["Resources Type"]: Resources_Type,
+    Description,
+    URL,
+    ["Contact Email"]: Contact_Email,
+    ["Contact Phone"]: Contact_Phone,
+    ["Contact Name"]: Contact_Name,
+    ["Image URL"]: Image_URL,
+  } = resource;
+
   return (
     <>
-      <div className="flex-col bg-teal-500 w-[265px]">
-        <img className="w-[265px] h-[187px]" src={imgURL} />
-        <h3 className="font-bold mt-[15px] mx-[3px]">{Name}</h3>
-        <p className="font-thin mx-[3px]">{Resources_Type}</p>
-        <p className="mt-[5px] mx-[3px]">{Description}</p>
-        <a href={URL}>
-          <button className="mt-[3px] mx-[3px] bg-[#C96C86] border-[2px] border-[#C96C86] rounded-2xl w-[134px]">Learn More</button>
+      <div className='flex-col bg-teal-500 w-[265px]'>
+        <img
+          className='w-[265px] h-[187px]'
+          src={Image_URL ?? "/womenWithClipboard.jpg"}
+        />
+        <h3 className='font-bold mt-[15px] mx-[3px]'>{Name}</h3>
+        <p className='font-thin mx-[3px]'>{Resources_Type}</p>
+        <p className='mt-[5px] mx-[3px]'>{Description}</p>
+        <a href={URL.startsWith("https://") ? URL : "https://".concat(URL)}>
+          <button className='mt-[3px] mx-[3px] bg-[#C96C86] border-[2px] border-[#C96C86] rounded-2xl w-[134px]'>
+            Learn More
+          </button>
         </a>
       </div>
     </>
-  )
+  );
 }
