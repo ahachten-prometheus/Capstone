a08f22df11bc68f623e6f3cced6988c65a0932ba
Mekhi-M <mekhim0731@gmail.com>
2025-06-14 12:01:57 -0400

    Refactoring + Commenting API Route
    Looked to minimize the length of filter formula string to avoid later errors.

--- src/app/api/resources/filter/route.js
+++ src/app/api/resources/filter/route.js
@@ -3,7 +3,13 @@ import { getRecords } from "@/services/airtable";
 export async function GET(req) {
   const params = req.nextUrl.searchParams;
 
+  // //////////////
+  // Filter Testing
+  // //////////////
+
   // params.append("pageSize", 100);
+  // params.append("filters", ["Category", "Getting the Help You Need"]);
+  // params.append("filters", ["Resources Type", "Crisis Call Lines"]);
   // params.append("filters", ["Subject", "Anxiety & Depression"]);
   // params.append("filters", ["Subject", "Bipolar Disorder"]);
   // params.append("filters", ["Subject", "Eating Disorder"]);
@@ -19,14 +25,22 @@ export async function GET(req) {
     const value = filter.slice(idxToSplit + 1);
 
     if (field === "Subject") {
-      // TODO
-      // Test RegEx matching
-      const regex = value.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-      // console.log(regex);
+      /** RegEx Intentions:
+       * Case-Insensitivity - Capitalization inconsistencies will match.
+       * Partial Word Matching - If the term is inside the string, it will match.
+       *                          Ex. "apple" WILL MATCH "apple pie" AND "pineapple".
+       * Exact Phrase Matching - If multiple words are included, we do not break them apart and look to match the whole phrase.
+       *                          Ex. "red car" WILL NOT MATCH "red bike", "blue car", or "My car is red". It WILL MATCH "My red car".
+       */
 
-      return `REGEX_MATCH(LOWER({${field}}),LOWER('(${regex})'))`;
+      // Escape special RegEx characters in the input. Every character is treated literally.
+      // Ex. "How. How??? How're you doing this?" --> "How\. How\?\?\? How're you doing this\?"
+      const regex = value.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+
+      // Case-Insensitivity is implemented by using LOWER() on both strings
+      return `REGEX_MATCH(LOWER({${field}}),LOWER('${regex}'))`;
     } else {
-      return `{${field}} = '${value}'`;
+      return `{${field}}='${value}'`;
     }
   });
 
