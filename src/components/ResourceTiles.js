111f86a7d668a58ffbe6c4967477ddc06e52056c
Mekhi-M <mekhim0731@gmail.com>
2025-06-14 23:17:34 -0400

    Error Handling: Resource Tiles
    Handled cases in which there is no data in the URL field

--- src/components/ResourceTiles.js
+++ src/components/ResourceTiles.js
@@ -21,11 +21,13 @@ export default function ResourceTiles({ resource }) {
         <h3 className='font-bold mt-[15px] mx-[3px]'>{Name}</h3>
         <p className='font-thin mx-[3px]'>{Resources_Type}</p>
         <p className='mt-[5px] mx-[3px]'>{Description}</p>
-        <a href={URL.startsWith("https://") ? URL : "https://".concat(URL)}>
-          <button className='mt-[3px] mx-[3px] bg-[#C96C86] border-[2px] border-[#C96C86] rounded-2xl w-[134px]'>
-            Learn More
-          </button>
-        </a>
+        {URL && (
+          <a href={URL.startsWith("https://") ? URL : "https://".concat(URL)}>
+            <button className='mt-[3px] mx-[3px] bg-[#C96C86] border-[2px] border-[#C96C86] rounded-2xl w-[134px]'>
+              Learn More
+            </button>
+          </a>
+        )}
       </div>
     </>
   );
