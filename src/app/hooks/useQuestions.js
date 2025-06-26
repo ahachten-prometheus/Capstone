cdc6cea3aecf70be750538cf41f1224cfc7c5802
Kevin <mrkev63@gmail.com>
2025-06-26 11:18:46 -0400

    navigation is done

--- src/app/hooks/useQuestions.js
+++
@@ -1,8 +0,0 @@
-import { surveyQuestions } from '@/data/questions';
-import { useState, useEffect } from 'react';
-
-export function useQuestion(value, next) {
-  useEffect(() => {
-    console.log(value)
-  }, [value])
-}
