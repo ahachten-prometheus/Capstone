8db9208f1c6801ba967e0f3d3164528268610afa
Sleepy-kookoo <americaxalvarez@gmail.com>
2025-07-08 10:52:47 -0400

    Created changes to styling based on Andrew Feedback

--- src/components/EventTilesGrid.js
+++ src/components/EventTilesGrid.js
@@ -2,7 +2,7 @@ import EventTile from './EventTile';
 
 export default function EventTilesGrid({ eventList }) {
 	return (
-		<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 px-6 sm:px-8">
+		<div className="grid gap-10 px-6 sm:px-8" style={{ gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 1fr))'}}>
 			{eventList?.length > 0 ? (
 				eventList.map((theEvent) => (
 					<EventTile key={theEvent.id} event={theEvent} />
